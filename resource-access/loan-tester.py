90fe779895361732000c936c9f0526f84b64152e
enettifee <enettifee@gmail.com>
2022-06-22 09:41:55 -0400

    Update loan-tester.py
    Minor speed improvements, checking for missing values, added in overdue fine and lost item functionality

--- resource-access/loan-tester.py
+++ resource-access/loan-tester.py
@@ -15,7 +15,6 @@ import csv
 import sys
 from datetime import datetime
 
-
 ## Set up variables for use in the script
 ## If you are repurposing this for another institution, you'll want to add the 
 ## appropriate okapi URLs, tenant names, tokens, etc. and make sure
@@ -41,22 +40,6 @@ snapshot2PostHeaders = {
     'x-okapi-token': snapshot2Token,
     'Content-Type': 'application/json'
 }
-
-# api calls
-# this is where we specify the values we use in constructing API calls later on in the script
-# there is a permissions issue with the test environment and the overdue and lost item policies API,
-# so that is commented out until it can be resolved.
-
-apiValues = {'loanPolicyId': '/circulation/rules/loan-policy?',
-             'noticePolicyId': '/circulation/rules/notice-policy?', 'requestPolicyId': '/circulation/rules/request-policy?'}
-
-policyNameApis = {'loanPolicyId': '/loan-policy-storage/loan-policies/',
-                  'noticePolicyId': '/patron-notice-policy-storage/patron-notice-policies/', 'requestPolicyId': '/request-policy-storage/request-policies/'}
-
-# there's a permission error here in Kiwi - let's just remove these for now and come back to it after I figure that out.
-#
-# '/circulation/rules/overdue-fine-policy?', '/circulation/rules/lost-item-policy?'}
-
 
 ## Now you can start asking for input from the person running the script
 ## They need to specify the name of the server they want to test on
@@ -159,62 +142,90 @@ for count, row in enumerate(testLoanScenarios):
     # pull patron_type_id friendly name
     for i in patronGroupsJson['usergroups']:
         if i['id'] == patron_type_id:
-            patronGroupFriendlyName = i['group']
-            friendlyResults['patron_group'] = patronGroupFriendlyName
+            friendlyResults['patron_group'] = i['group']
+    if not 'patron_group' in friendlyResults:
+        friendlyResults['patron_group'] = "Patron group not found"
+
 
     # pull loan type friendly name
     for i in loanTypesJson['loantypes']:
         if i['id'] == loan_type_id:
-            loanTypeFriendlyName = i['name']
-            friendlyResults['loan_type'] = loanTypeFriendlyName
+            friendlyResults['loan_type'] = i['name']
+    if not 'loan_type' in friendlyResults:
+        friendlyResults['loan_type'] = "Loan type not found"
+    
 
     # pull material type friendly name (API refers to it as item_type_id) 
     for i in materialTypesJson['mtypes']:
         if i['id'] == item_type_id:
-            materialTypeFriendlyName = i['name']
-            friendlyResults['material_type'] = materialTypeFriendlyName
+            friendlyResults['material_type'] = i['name']
+    if not 'material_type' in friendlyResults:
+        friendlyResults['material_type'] = "Material type not found"
 
-    # pull location friendly name
+    # pull location friendly name - using location code since a lot of our location names have commas in them
+    # which makes working with CSV a little too messy
+    #
+    # also pulling library friendly name so that it can be used in sorting/reviewing results in the
+    # output file
+    
     for i in locationsJson['locations']:
-        if i['id'] == location_id:
-            locationFriendlyName = i['name']
-            friendlyResults['location'] = locationFriendlyName
+        if i['id'] == location_id: # once you find the location ....
+            for j in librariesJson['loclibs']: # use the location to search your stored copy of the libraries Json
+                if i['libraryId'] == j['id']: # to find the associated library
+                    friendlyResults['library_name'] = j['name'] # and pull the name
+            friendlyResults['location'] = i['code'] # finally, add the location code so that it shows up in that order in the output file.
+    if not 'library_name' in friendlyResults:
+        friendlyResults['libraryName'], friendlyResults['location'] = "Library not found", "Location not found"
+    if not 'location' in friendlyResults:
+        friendlyResults['location'] = "Location not found"
+
 
     # now, you'll use the UUID values to query the APIs, get the results back, and then form
     # the full row in friendlyResults with the friendly names
-
-    for each in apiValues:
-        url = '{}{}{}{}{}{}{}{}{}{}'.format(testServer, apiValues.get(each), 'loan_type_id=', loan_type_id, '&item_type_id=', item_type_id, '&patron_type_id=', patron_type_id, '&location_id=', location_id)
-        postEach = requests.get(url, headers=postHeaders)
-        postEachVar = postEach.json()
-        if each == 'loanPolicyId':
-            for i in loanPoliciesJson['loanPolicies']:
-                if i['id'] == postEachVar['loanPolicyId']:
-                    friendlyResults['loanPolicy'] = i['name']
-        if each == 'noticePolicyId':
-            for i in noticePoliciesJson['patronNoticePolicies']:
-                if i['id'] == postEachVar['noticePolicyId']:
-                    friendlyResults['noticePolicy'] = i['name']
-        if each == 'requestPolicyId':
-            for i in requestPoliciesJson['requestPolicies']:
-                if i['id'] == postEachVar['requestPolicyId']:
-                   friendlyResults['requestPolicies'] = i['name']
-                   
-        # Can't test these until I resolve the permissions issue, but still setting up framework for now.
-        
-        # if each == 'overdueFinePolicyId':
-        #     for i in overduePoliciesJson['overdueFinePolicies']:
-        #         if i['id'] == postEachVar['overdueFinePolicyId']:
-        #             friendlyResults['overdueFinePolicies'] = i['name']       
-        # if each == 'lostItemFeePolicyId':
-        #     for i in lostItemPoliciesJson['lostItemFeesPolicies']:
-        #         if i['id'] == postEachVar['lostItemFeePolicyId']:
-        #             friendlyResults['lostItemPolicy'] = i['name']
-        
-        
-    # now write the results of the dictionary to a csv file
-    # the file will be created with the name friendlyOutput + a date time string
     
+    # first, let's make the URLs
+    
+    urlLoanPolicy = '{}{}{}{}{}{}{}{}{}{}'.format(testServer, '/circulation/rules/loan-policy?' , 'loan_type_id=', loan_type_id, '&item_type_id=', item_type_id, '&patron_type_id=', patron_type_id, '&location_id=', location_id)
+    urlRequestPolicy = '{}{}{}{}{}{}{}{}{}{}'.format(testServer, '/circulation/rules/request-policy?' , 'loan_type_id=', loan_type_id, '&item_type_id=', item_type_id, '&patron_type_id=', patron_type_id, '&location_id=', location_id)
+    urlNoticePolicy = '{}{}{}{}{}{}{}{}{}{}'.format(testServer, '/circulation/rules/notice-policy?' , 'loan_type_id=', loan_type_id, '&item_type_id=', item_type_id, '&patron_type_id=', patron_type_id, '&location_id=', location_id)
+    urlOverduePolicy = '{}{}{}{}{}{}{}{}{}{}'.format(testServer, '/circulation/rules/overdue-fine-policy?' , 'loan_type_id=', loan_type_id, '&item_type_id=', item_type_id, '&patron_type_id=', patron_type_id, '&location_id=', location_id)
+    urlLostItemPolicy = '{}{}{}{}{}{}{}{}{}{}'.format(testServer, '/circulation/rules/lost-item-policy?' , 'loan_type_id=', loan_type_id, '&item_type_id=', item_type_id, '&patron_type_id=', patron_type_id, '&location_id=', location_id)
+
+    # now, check all of the policies.
+    # 
+    # you could make one giant loop for this, but I found that it seemed like I got a bit of a performance improvement by 
+    # doing individual loops through the smaller chunks of data / discrete sections
+
+    postLoanPolicies = requests.get(urlLoanPolicy, headers=postHeaders)
+    postLoanPoliciesJson = postLoanPolicies.json()
+    for i in loanPoliciesJson['loanPolicies']:
+        if i['id'] == postLoanPoliciesJson['loanPolicyId']:
+            friendlyResults['loanPolicy'] = i['name']
+
+    postRequestPolicies = requests.get(urlRequestPolicy, headers=postHeaders)
+    postRequestPoliciesJson = postRequestPolicies.json()
+    for i in requestPoliciesJson['requestPolicies']:
+        if i['id'] == postRequestPoliciesJson['requestPolicyId']:
+            friendlyResults['requestPolicy'] = i['name']
+    
+    postNoticePolicies = requests.get(urlNoticePolicy, headers=postHeaders)
+    postNoticePoliciesJson = postNoticePolicies.json()
+    for i in noticePoliciesJson['patronNoticePolicies']:
+        if i['id'] == postNoticePoliciesJson['noticePolicyId']:
+            friendlyResults['noticePolicy'] = i['name']
+            
+    postOverduePolicies = requests.get(urlOverduePolicy, headers=postHeaders)
+    postOverduePoliciesJson = postOverduePolicies.json()
+    for i in overduePoliciesJson['overdueFinePolicies']:
+        if i['id'] == postOverduePoliciesJson['overdueFinePolicyId']:
+            friendlyResults['overduePolicy'] = i['name']
+            
+    postLostItemPolicies = requests.get(urlLostItemPolicy, headers=postHeaders)
+    postLostItemPoliciesJson = postLostItemPolicies.json()
+    for i in lostItemPoliciesJson['lostItemFeePolicies']:
+        if i['id'] == postLostItemPoliciesJson['lostItemPolicyId']:
+            friendlyResults['lostItemPolicy'] = i['name']
+
     with open("friendlyOutput-%s.csv" % startTime.strftime("%d-%m-%Y-%H%M%S"), 'a', newline='') as output_file:
          test_file = csv.writer(output_file)
          test_file.writerow(friendlyResults.values())
