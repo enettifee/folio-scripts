fc6df168ff79d06d075c97d9126fd62ab219ca81
enettifee <enettifee@gmail.com>
2022-12-02 14:27:49 -0500

    Adding new work
    Updating with continued work on script

--- course-reserves/main.py
+++ course-reserves/main.py
@@ -1,44 +1,83 @@
 """
-Basic python script to authenticate to a FOLIO
-instance and produce a CSV file with information
-about items currently on reserve for a defined time period.
+Basic python script to authenticate to a FOLIO instance and produce a CSV file with information
+about items currently on reserve for a defined reserves term.
 """
 
 import json
 import requests
 import configparser
+import csv
+from datetime import datetime
+
 
 # read in configuration files
-
 envConfig = configparser.ConfigParser()
 envConfig.read('config-template.ini')
 print(envConfig.sections())
 
 folioenv = input("Which environment are we retrieving course information from? ")
 fetchserver = envConfig[folioenv]['okapi_url']
-print(fetchserver)
 
 fetchHeaders = {
     'x-okapi-tenant': envConfig[folioenv]['tenant_id'],
     'x-okapi-token': envConfig[folioenv]['password']
 }
 
-
-# retrieve the current terms
-termsurl = '{}{}'.format(fetchserver, '/coursereserves/terms')
+# retrieve the current course terms from the FOLIO instance
+termsurl = '{}{}'.format(fetchserver, '/coursereserves/terms?limit=100')
 terms = requests.get(termsurl, headers=fetchHeaders)
 termsjson = terms.json()
 
 # ask which term they want to get course lists for
-
 whichterm = input("Which term should we fetch reserves for? ")
 for i in termsjson['terms']:
     if i['name'] == whichterm:
         termId = i['id']
 
+# fetch the courses with the courselisting object for the specified term
+coursesurl = '{}{}{}{}'.format(fetchserver, '/coursereserves/courses?limit=10000&query=(courseListing.termId=', termId, ')' )
+courses = requests.get(coursesurl, headers=fetchHeaders)
+coursesvalue = courses.json()
+coursesjson = coursesvalue["courses"]
 
-# fetch the courselistings for the term
-# courselistingsurl = '{}{}'.format(fetchserver, '/coursereserves/courselistings?')
-courselistingsurl = "https://folio-snapshot-okapi.dev.folio.org/coursereserves/courselistings?query=(termId=cd1eb7ed-2704-4b6a-9037-d7f1a5351f6b)"
-courselistings = requests.get(courselistingsurl, headers=fetchHeaders)
-print(courselistings.text)
+# fetch the reserves with the courseListingId for the specified term
+reservesurl = '{}{}{}{}'.format(fetchserver, '/coursereserves/reserves?expand=*?limit=10000&query=(courseListing.termId=', termId, ')' )
+reserves = requests.get(reservesurl, headers=fetchHeaders)
+reservesvalue = reserves.json()
+reservesjson = reservesvalue["reserves"]
+
+# loop through each reserve object returned and print information about the reserve and the associated course
+
+reservesInfoTest = {}
+reservesInfoTestList = []
+
+# use datetime to produce a random filename - I'm sure there are better ways to do this.
+filetime = datetime.now()
+
+# each item in reservejson represents an item on reserve for the specified term.
+# here, we initialize a dictionary file to store information about the reserve, pull out
+# specific information, and store it in that dictionary file. 
+# 
+# then, we dump the contents of that dictionary file into the CSV that is ultimately produced by the script.
+
+for each in reservesjson:
+    reservesInfoTest = {}
+    reservesInfoTest["id"] = each["id"]
+    reservesInfoTest["itemBarcode"] = each["copiedItem"]["barcode"]
+    reservesInfoTest["title"] = each["copiedItem"]["title"]
+    reservesInfoTest["courseListingId"] = each["courseListingId"]
+    for course in coursesjson:
+        if course["courseListingId"] == reservesInfoTest["courseListingId"]:
+            reservesInfoTest["courseName"] = course["name"]
+            reservesInfoTest["departmentName"] = course["departmentObject"]["name"]
+            break
+        else:
+            reservesInfoTest["courseName"] = "not found"
+    reservesInfoTestList.append(reservesInfoTest)
+
+
+    with open("friendlyOutput-%s.csv" % filetime.strftime("%d-%m-%Y-%H%M%S"), 'a', newline='') as output_file:
+        test_file = csv.writer(output_file)
+        test_file.writerow(reservesInfoTest.values())
+
+print(reservesInfoTestList)
